52e55053cd905629903f984fd1289de32fb119f9
liujinrui <pppap123@outlook.com>
2019-12-26 15:35:21 +0800

    给ChannelContext添加几个常用方法

--- src/main/java/com/pppppap/rpc/ChannelContext.java
+++ src/main/java/com/pppppap/rpc/ChannelContext.java
@@ -1,9 +1,12 @@
 package com.pppppap.rpc;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
 /**
- * TODO
+ * 连接上下文，用来发送数据和获取连接信息
  *
  * @author liujinrui
  * @since 2019/12/26 12:03
@@ -15,7 +18,33 @@ public class ChannelContext {
         this.channel = channel;
     }
 
-    void send(byte[] bytes) {
+    public SocketChannel channel() {
+        return this.channel;
+    }
 
+    public InetSocketAddress remoteAddress() {
+        try {
+            return (InetSocketAddress) channel.getRemoteAddress();
+        } catch (IOException e) {
+            throw new RpcException(e);
+        }
+    }
+
+    public InetSocketAddress localAddress() {
+        try {
+            return (InetSocketAddress) channel.getLocalAddress();
+        } catch (IOException e) {
+            throw new RpcException(e);
+        }
+    }
+
+    public void send(byte[] bytes) {
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        buffer.flip();
+        try {
+            channel.write(buffer);
+        } catch (IOException e) {
+            throw new RpcException("发送数据失败", e);
+        }
     }
 }
