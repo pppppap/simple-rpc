9cab9f5da01765afa6ee4afefbda923e592f7fad
liujinrui <pppap123@outlook.com>
2019-12-26 15:51:05 +0800

    添加注释

--- src/main/java/com/pppppap/rpc/codec/Codec.java
+++ src/main/java/com/pppppap/rpc/codec/Codec.java
@@ -1,13 +1,25 @@
 package com.pppppap.rpc.codec;
 
 /**
- * TODO
+ * 编解码器
  *
  * @author liujinrui
  * @since 2019/12/25 10:10
  */
 public interface Codec {
+    /**
+     * 把对象编码
+     *
+     * @param o 对象
+     * @return 字节数组
+     */
     byte[] code(Object o);
 
+    /**
+     * 从字节数组反序列化为对象
+     *
+     * @param bytes 字节数组
+     * @return 对象
+     */
     Object decode(byte[] bytes);
 }
