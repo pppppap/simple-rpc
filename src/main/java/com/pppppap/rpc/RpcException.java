dd12d1657a451aaa5f2479eeb6a72d17522f4fe0
liujinrui <pppap123@outlook.com>
2019-12-26 11:43:37 +0800

    添加一个公共异常类

---
+++ src/main/java/com/pppppap/rpc/RpcException.java
@@ -0,0 +1,25 @@
+package com.pppppap.rpc;
+
+/**
+ * TODO
+ *
+ * @author liujinrui
+ * @since 2019/12/26 11:39
+ */
+public class RpcException extends RuntimeException {
+    public RpcException(String message) {
+        super(message);
+    }
+
+    public RpcException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RpcException(Throwable cause) {
+        super(cause);
+    }
+
+    protected RpcException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
